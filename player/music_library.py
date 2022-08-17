8159d87d1ba19817d5683e3714bd2f9ed145f1ed
Jimmy Lyons <jimmylyons1@hotmail.co.uk>
2022-08-17 09:59:15 +0100

    green: test_initialses_with_empty_library

--- player/music_library.py
+++ player/music_library.py
@@ -1,2 +1,3 @@
 class MusicLibrary:
-    pass
+    def __init__(self):
+        self._library = {}
--- tests/test_music_library.py
+++ tests/test_music_library.py
@@ -7,3 +7,7 @@ class TestMusicLibrary(unittest.TestCase):
     def test_constructs(self):
         music_library = MusicLibrary()
         self.assertIsInstance(music_library, MusicLibrary)
+
+    def test_initialses_with_empty_library(self):
+        music_library = MusicLibrary()
+        self.assertEqual(getattr(music_library, '_library'), {})
